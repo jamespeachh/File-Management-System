cb83721d06857eaed6d4099998fa62106baaf5f1
Matthew Lewis <mlewis@setonhill.edu>
2024-01-16 15:31:27 -0500

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -11,32 +11,57 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| FOR NOW, i am putting all the literal routes into groups, not the ones
+| that "should" be in different places but are not right now.
 |
+| If something routes to directory right now, then it will be under:
+| "Directory routes"
 */
 
+/*
+|--------------------------------------------------------------------------
+| Directory
+|--------------------------------------------------------------------------
+*/
 Route::get('/', [DirectoryController::class, 'index'])->name('directory');//->middleware('auth');
 Route::get('/home', [DirectoryController::class, 'index'])->name('home');//->middleware('auth');
-
-// book directory
-Route::get('/directory', [DirectoryController::class, 'index'])
-    ->name('directory');//->middleware('auth');
+Route::get('/directory', [DirectoryController::class, 'index'])->name('directory');//->middleware('auth');
 
 // Route::get('/import', [ImportController::class, 'index'])->name('import');
 
-
+/*
+|--------------------------------------------------------------------------
+| Books
+|--------------------------------------------------------------------------
+*/
 //testing out a new book path
 Route::get('/book/{bookName}/{pageNumber}', [BookController::class, 'index'])
     ->name('book');//->middleware('auth');
 Route::get('/book/{bookName}', [BookController::class, 'indexNoVar']);//->middleware('auth');
-Route::get('/calander', function(){
-    return view('calanders');
-});//->middleware('auth');
 
+/*
+|--------------------------------------------------------------------------
+| Calendar
+|--------------------------------------------------------------------------
+*/
+//Route::get('/calander', function(){
+//    return view('calanders');
+//});//->middleware('auth');
+
+/*
+|--------------------------------------------------------------------------
+| IMPORT
+|--------------------------------------------------------------------------
+*/
+Route::get('/import', [ImportController::class, 'showUploadForm'])->name('upload.form');
+Route::post('/submit-form', [ImportController::class, 'submitForm'])->name('submit-form');
+
+/*
+|--------------------------------------------------------------------------
+| TESTING
+|--------------------------------------------------------------------------
+*/
 Route::get('/test', [TestController::class, 'index'])
     ->name('test');
 
-Route::get('/upload', [ImportController::class, 'showUploadForm'])->name('upload.form');
-Route::post('/submit-form', [ImportController::class, 'submitForm'])->name('submit-form');
+
