05dfdda5d09b984fc8f99e2034cc713ed5e83110
Matthew Lewis <mlewis@setonhill.edu>
2024-01-17 01:34:21 -0500

    make form fields required

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -10,14 +10,14 @@
 <body>
 <form method="POST" action="{{ route('submit-form') }}" class="import-form">
     @csrf
-    <input type="text" placeholder="bookName" name="bookName" class="form-inputs"><br>
-    <input type="text" placeholder="url" name="url" class="form-inputs"><br>
-    <input type="text" placeholder="title formatted" name="title_formatted" class="form-inputs"><br>
-    <input type="text" placeholder="pages" name="pages" class="form-inputs"><br>
-    <input type="text" placeholder="img src" name="img_src" class="form-inputs"><br>
-    <input type="text" placeholder="img alt" name="img_alt" class="form-inputs"><br>
-    <input type="file" placeholder="Cover image" name="cover_img" class="form-inputs" accept=".png,.jpeg,.jpg"><br>
-    <input type="text" placeholder="password" name="password" class="form-inputs"><br>
+    <input type="text" placeholder="bookName" name="bookName" class="form-inputs" required><br>
+    <input type="text" placeholder="url" name="url" class="form-inputs" required><br>
+    <input type="text" placeholder="title formatted" name="title_formatted" class="form-inputs" required><br>
+    <input type="text" placeholder="pages" name="pages" class="form-inputs" required><br>
+    <input type="text" placeholder="img src" name="img_src" class="form-inputs" required><br>
+    <input type="text" placeholder="img alt" name="img_alt" class="form-inputs" required><br>
+    <input type="file" placeholder="Cover image" name="cover_img" class="form-inputs" accept=".png,.jpeg,.jpg" required><br>
+    <input type="text" placeholder="password" name="password" class="form-inputs" required><br>
     <button>submit</button>
 </form>
 </body>
