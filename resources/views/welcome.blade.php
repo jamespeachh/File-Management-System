8ec67e5aa895f78e6912a5d9f1caef71fbfabcf3
jamespeachh <matthlewis24@gmail.com>
2024-02-11 12:11:11 -0500

    change welcome title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Welcome!</title>
+        <title>Welcome to YCL</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
