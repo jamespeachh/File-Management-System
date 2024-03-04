59fbf44cfab5cb461731efab13dcd31b73d743bb
jamespeachh <matthlewis24@gmail.com>
2024-03-04 12:42:27 -0500

    test on different computer

--- resources/views/calanders.blade.php
+++ resources/views/calanders.blade.php
@@ -8,5 +8,7 @@
                 <!-- <iframe  src="https://calendar.google.com/calendar/embed?src=m.lewis%40setonhill.edu&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>     -->
             </div>
         </div>
+
+
     
 </body>
