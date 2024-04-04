1a4f9945061716fec1bd488008864ba2f764d817
jamespeachh <matthlewis24@gmail.com>
2024-04-04 14:47:57 -0400

    add books table migration

---
+++ database/migrations/2024_04_04_183800_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 32);
+            $table->string('formatted_title', 64);
+            $table->integer('pages');
+            $table->string('cover_pic', 32);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
