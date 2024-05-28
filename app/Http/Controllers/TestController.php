0f7c37ac1bdbcf74f6656470763c0817f4c47e68
jamespeachh <matthlewis24@gmail.com>
2024-05-28 13:10:09 -0400

    check new features

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -26,10 +26,10 @@ class TestController extends Controller
     public function index(Request $request)
     {
         $b = new book();
-//        $books = $b->query()->select()
-//            ->get()
-//            ->toArray();
-        $books = [["id"=>1,"formatted_title"=>"book 1"],["id"=>2,"formatted_title"=>"book 2"],["id"=>3,"formatted_title"=>"book 3"],["id"=>4,"formatted_title"=>"book 4"]];
+        $books = $b->query()->select()
+            ->get()
+            ->toArray();
+//        $books = [["id"=>1,"formatted_title"=>"book 1"],["id"=>2,"formatted_title"=>"book 2"],["id"=>3,"formatted_title"=>"book 3"],["id"=>4,"formatted_title"=>"book 4"]];
         return view('test', ['books'=>$books]);
     }
 
@@ -40,32 +40,31 @@ class TestController extends Controller
 
 //        dd("working");
 
-        // Validate the request
-//        dump(
-//        $request->validate([
-//            'onSite' => 'required|string',
-//            'book' => 'nullable|integer',
-//            'title' => 'nullable|string|max:64',
-//            'author' => 'nullable|string|max:64',
-//            'summary' => 'nullable|string|max:255',
-//            'addBook' => 'nullable|boolean',
-//            'read' => 'nullable|boolean',
-//            'rateqm' => 'nullable|boolean',
-//            'rate' => 'nullable|numeric|min:0|max:10',
-//        ])
-//        );
+        $onSite = $request->input('onSite') === 'yes' ? 1 : 0;
+        $bookID = $request->input('book');
+        $title = $request->input('title');
+        $author = $request->input('author');
+        $summary = $request->input('summary');
+        $addBook=$request->input('addBook', false) ? 1 : 0;
+        if($onSite == 1){
+            $b = new book();
+            $title = $b->query()->select()->where("id",$bookID)->get()->toArray()[0];
+            $author = "";
+            $summary="";
+            $addBook=0;
+        }
 
         // Prepare the data for insertion
         dump(
         $data = [
             'list_id' => $request->input('list_id'), // Assuming you have a list_id in your form
             'user_id' => auth()->id(), // Assuming the user is authenticated
-            'on_site' => $request->input('onSite') === 'yes' ? 1 : 0,
-            'book_id' => $request->input('book'),
-            'title' => $request->input('title'),
-            'author' => $request->input('author'),
-            'summary' => $request->input('summary'),
-            'want_book_added' => $request->input('addBook', false) ? 1 : 0,
+            'on_site' => $onSite,
+            'book_id' => $bookID,
+            'title' => $title,
+            'author' => $author,
+            'summary' => $summary,
+            'want_book_added' => $addBook,
             'status' => $request->input('read') ? 'read' : 'not read',
             'rating' => $request->input('rateqm') ? $request->input('rate') : null,
         ]);
