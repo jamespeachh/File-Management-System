ca19b2e4fdc9b45594021a7d899eacb86c28047b
Matthew Lewis <mlewis@setonhill.edu>
2024-06-03 08:32:49 -0400

    change all the stylings to get a better layout

--- resources/views/bladeTemplates/summer-reading-list.blade.php
+++ resources/views/bladeTemplates/summer-reading-list.blade.php
@@ -23,22 +23,6 @@
         input[type="range"] {
             width: 100%;
         }
-        .stars span {
-            display: block;
-            left: 0;
-            overflow: hidden;
-            position: absolute;
-            top: 0;
-        }
-        .stars span:before {
-            content: "★★★★★";
-            position: absolute;
-            top: 0;
-            left: 0;
-            z-index: 1;
-            color: gold;
-            text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000; /* Black border */
-        }
         button {
             background-color: #cc0052;
             color: #fff;
@@ -46,7 +30,6 @@
             border-radius: 5px;
             padding: 10px 15px;
             cursor: pointer;
-            width: 100%;
         }
         button:hover {
             background-color: #99003d;
@@ -58,7 +41,8 @@
             border: 2px solid #cc0052;
             border-radius: 15px;
             padding: 20px;
-            width: 300px;
+            width: 75%;
+            margin: auto;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             color: #800000;
         }
@@ -74,6 +58,7 @@
             padding: 10px;
             margin-bottom: 10px;
             display: flex;
+            justify-content: space-between;
             align-items: center;
         }
         .stylish-list li:last-child {
@@ -104,9 +89,28 @@
             font-size: 25px;
             height: 25px;
             width: 125px;
-            margin: 0 auto 15px auto;
+            margin: 10px 0;
             position: relative;
             padding: 0;
+        }
+        .stars span {
+            display: block;
+            left: 0;
+            overflow: hidden;
+            position: absolute;
+            top: 0;
+        }
+        .stars span:before {
+            content: "★★★★★";
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 1;
+            color: gold;
+            text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000; /* Black border */
+        }
+        .book-details {
+            flex-grow: 1;
         }
     </style>
 </head>
@@ -118,14 +122,15 @@
         </li>
         @foreach($listItems as $listItem)
             <li>
-                <span class="list-item-text">{{$listItem['title']}}</span>
+                <div class="book-details">
+                    <span class="list-item-text">{{$listItem['title']}}</span>
+                    <div class="stars">
+                        <span style="width: {{($listItem['rating'] / 5) * 100}}%;">★★★★★</span>
+                    </div>
+                </div>
                 <div class="list-item-actions">
                     <button class="list-item-button" onclick="window.location.href='{{ url(route('reading.edit-item', ['listItemId'=>$listItem['id']])) }}'">Edit</button>
                     <button class="list-item-button">Delete</button>
-                </div>
-                <br>
-                <div class="stars">
-                    <span style="width: {{($listItem['rating'] / 10) * 100}}%;">★★★★★</span>
                 </div>
             </li>
         @endforeach
