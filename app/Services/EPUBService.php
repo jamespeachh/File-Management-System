36a85da0a9215a0dc7836f8eccdf6552ef69e59a
jamespeachh <matthlewis24@gmail.com>
2025-05-09 14:05:14 -0400

    fix method

--- app/Services/EPUBService.php
+++ app/Services/EPUBService.php
@@ -192,6 +192,16 @@ class EPUBService
         return $elements->length > 0 ? $elements->item(0)->nodeValue : null;
     }
 
+    private function getContainerXml()
+    {
+        return '<?xml version="1.0" encoding="UTF-8"?>
+<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+    <rootfiles>
+        <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
+    </rootfiles>
+</container>';
+    }
+
     private function getContentOpf($title, $author, $chapters)
     {
         $uuid = 'urn:uuid:' . Str::uuid();
